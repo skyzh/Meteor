8c1ed10e536145346fc213303bac02bd2bcb95ae
Alex Chi <iskyzh@gmail.com>
2019-12-07 19:06:27 +0800

    add chart view supporting wheel

--- src/MetroChartView.cpp
+++ src/MetroChartView.cpp
@@ -3,7 +3,15 @@
 //
 
 #include "MetroChartView.h"
+#include <QDebug>
+#include <QtMath>
 
 void MetroChartView::wheelEvent(QWheelEvent *event) {
-    chart()->scroll(event->pixelDelta().x(), event->pixelDelta().y());
+    chart()->scroll(-event->pixelDelta().x(), 0);
+    QChartView::wheelEvent(event);
+    emit request_data();
 }
+
+MetroChartView::MetroChartView(QtCharts::QChart *chart, QWidget *parent) : QChartView(chart, parent) {}
+
+MetroChartView::MetroChartView(QWidget *parent) : QChartView(parent) {}
--- src/MetroChartView.h
+++ src/MetroChartView.h
@@ -8,8 +8,17 @@
 #include <QChartView>
 
 class MetroChartView : public QtCharts::QChartView {
+    Q_OBJECT
 protected:
     void wheelEvent(QWheelEvent *event) override;
+
+public:
+    MetroChartView(QWidget *parent);
+
+    MetroChartView(QtCharts::QChart *chart, QWidget *parent);
+
+signals:
+    void request_data();
 };
 
 
